626d8b79e6d9960daddb8d29c1733ef33403b4dd
Juye Kweon <JuyeKweon@Juyes-MacBook-Pro.local>
2014-10-01 14:16:32 -0700

    Initial commit

---
+++ app.rb
@@ -0,0 +1,23 @@
+# app.rb
+
+# use bundler
+require 'rubygems'
+require 'bundler/setup'
+
+# load all of the gems in the gemfile
+Bundler.require
+
+# define a route for the root of the site
+get '/' do
+	file_contents = File.read('todo.txt')
+	@lists = file_contents.split("\n")
+	erb :todo
+end
+
+require 'json'
+post '/' do
+	JSON.pretty_generate params
+	File.open("todo.txt", "a") do |file|
+    file.puts "#{params[:task]} - #{params[:date]}"
+  end
+end
