88e2b1a29af4015d26185bc9b87f2fdc969ea2ac
Juye Kweon <JuyeKweon@Juyes-MacBook-Pro.local>
2014-11-04 23:00:49 -0800

    did it again to understand

--- app.rb
+++ app.rb
@@ -1,14 +1,15 @@
 # app.rb
+# this is a simple Sinatra example app
 
 # use bundler
 require 'rubygems'
 require 'bundler/setup'
-
 # load all of the gems in the gemfile
 Bundler.require
 
 # define a route for the root of the site
 get '/' do
+	# render the views/todo.erb template
 	file_contents = File.read('todo.txt')
 	@lists = file_contents.split("\n")
 	erb :todo
